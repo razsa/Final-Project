84d6d47dee56fca9173f29ea9a1bc0bad46db9a3
Razsa <21493e@gmail.com>
2020-05-04 01:53:48 -0400

    metamask web3 integration

---
+++ src/components/App.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+import Web3 from 'web3';
+
+class App extends Component {
+
+      async componentDidMount() { 
+	await this.loadWeb3()
+        await this.loadBlockchainData() 	
+      }
+      
+      async loadBlockchainData() {
+	      const web3 = window.web3
+	      const accounts = await web3.eth.getAccounts()
+      }
+      	
+      async loadWeb3() {
+	if (window.ethereum) {
+           window.web3 = new Web3(window.ethereum)
+           await window.ethereum.enable()
+        }
+        else if (window.web3) {
+          window.web3 = new Web3(window.web3.currentProvider)
+        }
+        else {
+         window.alert('Non-Ethereum browser detected. You should consider trying MetaMask!')
+        }
+      }
+     
+     render() {
+	return ( 
+	 <div>
+        <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
+          <a
+            className="navbar-brand col-sm-3 col-md-2 mr-0"
+            href="http://www.dappuniversity.com/bootcamp"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Take Share Game
+          </a>
+        </nav>
+        <div className="container-fluid mt-5">
+          <div className="row">
+            <main role="main" className="col-lg-12 d-flex text-center">
+              <div className="content mr-auto ml-auto">
+                <a
+                  href="http://www.dappuniversity.com/bootcamp"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                </a>
+                <h1>Hello World</h1>
+              </div>
+            </main>
+          </div>
+        </div>
+      </div>
+
+	);
+	}
+      }
+	
+ export default App;
